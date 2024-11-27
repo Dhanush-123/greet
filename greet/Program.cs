42a77e395f8bacca992a5bb2ce029384f53678c1
DHANUSH <114476777+Dhanush-123@users.noreply.github.com>
2024-11-27 15:04:42 +0530

    Add files via upload

---
+++ greet/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter your name: ");
+        string? name = Console.ReadLine();
+
+        DateTime currentTime = DateTime.Now;
+
+        Console.WriteLine($"Hello, {name}! Welcome, and the time is {currentTime:hh:mm tt}. ");
+
+    }
+}
+
